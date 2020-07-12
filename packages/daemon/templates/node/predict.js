9f2997bb46d9441797837c0559f9af5d0a4c15bf
yorkie <yorkiefixer@gmail.com>
2020-07-12 17:01:14 +0800

    daemon: add a inference.js in model-define plugin for inferencing.
    This simplifies the model-define plugin development.

--- packages/daemon/templates/node/predict.js
+++ packages/daemon/templates/node/predict.js
@@ -3,39 +3,66 @@
 const { pipeline, output } = require('./metadata.json');
 
 function _requirePlugin(name) {
-  const mod = require(pipeline[name]);
-  if (mod && typeof mod.default === 'function') {
-    return mod.default;
+  let modPath = pipeline[name];
+  let isInference = false;
+  if (name === 'modelDefine') {
+    try {
+      require.resolve(`${modPath}/dist/inference.js`);
+      modPath = `${modPath}/dist/inference.js`;
+      isInference = true;
+    } catch (e) {
+      console.warn(`${modPath}/dist/inference.js doesn't exist, use ${modPath}.`);
+    }
   }
-  return mod;
+  const mod = require(modPath);
+  const plugin = {
+    name,
+    isInference,
+    module: null
+  };
+  if (mod && typeof mod.default === 'function') {
+    plugin.module = mod.default;
+  } else {
+    plugin.module = mod;
+  }
+  return plugin;
 }
 
-const modelDefineModule = _requirePlugin('modelDefine');
+const modelDefine = _requirePlugin('modelDefine');
 
-let dataProcessModule;
+let dataProcess;
 if (typeof pipeline.dataProcess === 'string') {
-  dataProcessModule = _requirePlugin('dataProcess');
+  dataProcess = _requirePlugin('dataProcess');
 }
 
+const recoverPath = `${__dirname}/model`;
 const dataset = JSON.parse(output.dataset);
-const model = modelDefineModule(null, {
-  recoverPath: __dirname + '/model',
-  dataset
-});
+let model;
+if (!modelDefine.isInference) {
+  model = modelDefine.module(null, {
+    recoverPath,
+    dataset,
+  });
+} else {
+  // modelPath, { labelMaps, feature }
+  model = modelDefine.module(recoverPath, dataset);
+}
 
-function predict(data) {
+module.exports = function predict(data) {
   const sample = { data, label: null };
   let future = model;
 
-  if (typeof dataProcessModule === 'function') {
+  if (dataProcess && typeof dataProcess.module === 'function') {
     future = future.then((m) => {
-      dataProcessModule(sample, {}, JSON.parse(pipeline.dataProcessParams));
+      dataProcess.module(sample, {}, JSON.parse(pipeline.dataProcessParams));
       return m
     });
   }
   return future.then((m) => {
-    return m.predict(sample);
+    if (typeof m === 'function') {
+      return m(sample);
+    } else {
+      return m.predict(sample);
+    }
   });
 };
-
-module.exports = predict;
